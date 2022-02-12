ba035c453ab0e678bc26a1d3cf7429e8d256944e
Denys Andriukhov <andryhov.denis@gmail.com>
2022-02-12 22:52:19 +0200

    Refactor ActorsRecycleViewAdapter: remove ActorsClickListener, ActorViewHolder placed in a separate class

--- app/src/main/java/com/andriukhov/mymovies/adapter/ActorsRecyclerViewAdapter.kt
+++ app/src/main/java/com/andriukhov/mymovies/adapter/ActorsRecyclerViewAdapter.kt
@@ -1,20 +1,17 @@
 package com.andriukhov.mymovies.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.andriukhov.mymovies.R
+import com.andriukhov.mymovies.adapter.viewholder.ActorViewHolder
 import com.andriukhov.mymovies.pojo.Actor
-import com.andriukhov.mymovies.listener.ActorClickListener
 import com.squareup.picasso.Picasso
 
 class ActorsRecyclerViewAdapter :
-    RecyclerView.Adapter<ActorsRecyclerViewAdapter.ActorsViewHolder>() {
+    RecyclerView.Adapter<ActorViewHolder>() {
 
-    lateinit var actorClickListener: ActorClickListener
+    var onActorClick: ((Actor) -> Unit)? = null
 
     var actors = mutableListOf<Actor>()
         set(value) {
@@ -23,33 +20,21 @@ class ActorsRecyclerViewAdapter :
             notifyDataSetChanged()
         }
 
-    inner class ActorsViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        var imageViewActorPhoto: ImageView? = null
-        var textViewActorName: TextView? = null
-        var textViewCharacterName: TextView? = null
-
-        init {
-            imageViewActorPhoto = view.findViewById(R.id.imageViewActorPhoto)
-            textViewActorName = view.findViewById(R.id.textViewActorName)
-            textViewCharacterName = view.findViewById(R.id.textViewCharacterName)
-
-            view.setOnClickListener {
-                actorClickListener.onActorClickListener(adapterPosition)
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorsViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ActorViewHolder {
         val itemView = LayoutInflater.from(parent.context)
             .inflate(R.layout.actor_item, parent, false)
-        return ActorsViewHolder(itemView)
+        return ActorViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: ActorsViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ActorViewHolder, position: Int) {
         val actor = actors[position]
         Picasso.get().load(actor.getFullProfilePhotoPath()).placeholder(R.drawable.placeholder).into(holder.imageViewActorPhoto)
         holder.textViewActorName?.text = actor.name
         holder.textViewCharacterName?.text = String.format("(%s)", actor.character)
+
+        holder.itemView.setOnClickListener {
+            onActorClick?.invoke(actor)
+        }
     }
 
     override fun getItemCount(): Int = actors.size
